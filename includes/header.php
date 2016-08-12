cda261f275057790b26179414030c3fd4ed003d0
Delton M. Childs III <delton@deltonchilds.com>
2016-08-11 20:35:07 -0500

    Cookies and Sessions
    lynda.com tutorial on cookies and sessions, complete.

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,11 @@
+<?php require_once('includes/functions.php')?>
+<?php 
+	// sets cookie
+	$name = "test";
+	$value = "45";
+	$expire = time() + (60*60*24*7); //add seconds
+	//setcookie($name, $value, $expire);
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
