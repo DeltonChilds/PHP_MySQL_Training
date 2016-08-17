20a24e7df17dbef4ec5ed77b0e2830a61672e654
Delton M. Childs III <delton@deltonchilds.com>
2016-08-17 15:36:27 -0500

    Training Files To Archive
    this moves all training files to archive folder for future reference to
    establish a base of operations for the actual CMS.

---
+++ includes/layouts/footer.php
@@ -0,0 +1,16 @@
+<footer class="row">
+	<section class="12-large columns">
+		<p>
+			&copy; Delton Childs
+		</p>
+	</section>
+</footer>
+
+</body>
+</html>
+<?php
+	// close database connection
+	if (isset($connection)) {
+		mysqli_close($connection);
+	}
+?>
