68d2e4cb02ff927d28a31ed2a98cd8ac939f6f09
Delton M. Childs III <delton@deltonchilds.com>
2016-09-14 18:50:51 -0500

    Refactors the main navigation by moving it to the functions file, creating new function navigation() accepting two paramiters, the currently selected page and the currently selected subject, if any.

--- includes/functions.php
+++ includes/functions.php
@@ -1,9 +1,77 @@
 <?php
-
 function confirm_query($result_set) {
-	if (!$result_set) {
-		die("Database query failed.");
+	if (! $result_set) {
+		die ( "Database query failed." );
 	}
 }
-
+function find_all_subjects() {
+	global $connection;
+	$query = "SELECT * ";
+	$query .= "FROM subjects ";
+	$query .= "WHERE visible = 1 ";
+	$query .= "ORDER BY position ASC";
+	$subject_set = mysqli_query ( $connection, $query );
+	confirm_query ( $subject_set );
+	return $subject_set;
+}
+function find_pages_for_subject($subject_id) {
+	global $connection;
+	
+	$query = "SELECT * ";
+	$query .= "FROM pages ";
+	$query .= "WHERE visible = 1 ";
+	$query .= "AND subject_id = {$subject_id} ";
+	$query .= "ORDER BY position ASC";
+	$page_set = mysqli_query ( $connection, $query );
+	confirm_query ( $page_set );
+	return $page_set;
+}
+function navigation($selected_subject_id, $selected_page_id) {
+	$output = "<ul class=\"dropdown menu subjects\" data-dropdown-menu>";
+	$subject_set = find_all_subjects ();
+	
+	while ( $subject = mysqli_fetch_assoc ( $subject_set ) ) {
+		// output data from each row
+		
+		$output .= "<li";
+		if ($subject ["id"] == $selected_subject_id) {
+			$output .= " class=\"selected\"";
+		}
+		
+		$output .= ">";
+		
+		$output .= "<a href=\"manage_content.php?subject=";
+		$output .= urlencode ( $subject ['id'] ) . "\">";
+		$output .= $subject ['menu_name'] . "</a>";
+		
+		$page_set = find_pages_for_subject ( $subject ['id'] );
+		$num_rows = mysqli_num_rows ( $page_set );
+		if ($num_rows != 0) {
+			
+			$output .= "<ul class=\"menu pages\">";
+			
+			while ( $page = mysqli_fetch_assoc ( $page_set ) ) {
+										// output data from each row
+										
+	          $output .= "<li";
+	          		if ($page["id"] == $selected_page_id){
+	          		$output .= " class=\"selected\"";}
+					$output .= ">"; 
+	          
+	          $output .= "<a href=\"manage_content.php?page="; 
+	          $output .= urlencode($page['id']) . "\">";
+	          $output .= $page['menu_name'] . "</a></li>";
+	          } 
+	          $output .= "</ul>";
+	        mysqli_free_result($page_set); 
+	        
+								
+	}
+			
+	      $output .= "</li>";
+	      }
+	    $output .= "</ul>";
+	    mysqli_free_result($subject_set); 
+	 return $output;
+}
 ?>
--- manage_content.php
+++ manage_content.php
@@ -2,68 +2,60 @@
 require_once ('includes/db_connection.php');
 ?>
 <?php
-	require_once 'includes/functions.php';
+
+require_once 'includes/functions.php';
 ?>
 <?php
-	include_once 'includes/layouts/header.php';
+
+include_once 'includes/layouts/header.php';
+?>
+
+<?php
+
+if (isset ( $_GET ["subject"] )) {
+	$selected_subject_id = $_GET ["subject"];
+	$selected_page_id = null;
+} elseif (isset ( $_GET ["page"] )) {
+	$selected_page_id = $_GET ["page"];
+	$selected_subject_id = null;
+} else {
+	$selected_page_id = null;
+	$selected_subject_id = null;
+}
 ?>
 <nav class="row">
-  <div class="large-12 columns" >
-    <ul class="dropdown menu subjects" data-dropdown-menu >
-      <?php $query = "SELECT * ";
-	$query .= "FROM subjects ";
-	$query .= "WHERE visible = 1 ";
-	$query .= "ORDER BY position ASC";
-
-	$subject_set = mysqli_query($connection, $query);
-	// Test if there was a querry error.
-	
-	confirm_query($subject_set);
-	
-?>
-      <?php
-				while ($subject = mysqli_fetch_assoc($subject_set)) {
-					// output data from each row
-					?>
-      <li><a href=""> <?php echo $subject['menu_name'] ?> </a>
-        <?php $query = "SELECT * ";
-				$query .= "FROM pages ";
-				$query .= "WHERE visible = 1 ";
-				$query .= "AND subject_id = {$subject['id']} ";
-				$query .= "ORDER BY position ASC"; 
-				$page_set = mysqli_query($connection, $query);
-				confirm_query($page_set);
-					
-					$num_rows = mysqli_num_rows($page_set);
-					
-					if($num_rows != 0){ ?>
-        <ul class="menu pages">
-          <?php 
-						while ($page = mysqli_fetch_assoc($page_set)) {
-					// output data from each row
-					?>
-          <li><a href="#"> <?php echo $page['menu_name'] ?> </a></li>
-          <?php } ?>
-        </ul>
-        <?php mysqli_free_result($page_set); ?>
-        <?php	}
-				?>
-      </li>
-      <?php } ?>
-    </ul>
-    <?php mysqli_free_result($subject_set); ?>
+	<div class="large-12 columns">
+		<?php echo navigation($selected_subject_id, $selected_page_id); ?>
   </div>
 </nav>
 <div class="row">
-  <section class="large-4 medium-2 small-12 columns">
-    <h2>Manage Content</h2>
-    <p> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. </p>
-  </section>
-  <section class="large-8 medium-10 small-12 columns">
-    <h2>HTML Ipsum</h2>
-    <p> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus </p>
-  </section>
+	<section class="large-4 medium-6 small-12 columns">
+		<h2>Manage Content</h2>
+		<ul>
+			<li><?php echo (isset($selected_subject_id) ? $selected_subject_id : "No Subject Selected") ?></li>
+			<li><?php echo (isset($selected_page_id) ? $selected_page_id : "No Page Selected") ?></li>
+		</ul>
+		<p>Pellentesque habitant morbi tristique senectus et netus et
+			malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat
+			vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+			amet quam egestas semper. Aenean ultricies mi vitae est.</p>
+	</section>
+	<section class="large-8 medium-6 small-12 columns">
+		<h2>HTML Ipsum</h2>
+		<p>Pellentesque habitant morbi tristique senectus et netus et
+			malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat
+			vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+			amet quam egestas semper. Aenean ultricies mi vitae est. Mauris
+			placerat eleifend leo. Quisque sit amet est et sapien ullamcorper
+			pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae,
+			ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt
+			condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac
+			dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent
+			dapibus, neque id cursus faucibus, tortor neque egestas augue, eu
+			vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi,
+			tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+	</section>
 </div>
 <?php
-	include_once 'includes/layouts/footer.php';
+include_once 'includes/layouts/footer.php';
 ?>
